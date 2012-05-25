d82bd2d29ddd106e8a189ff0b07c0b82e3ac8811
Nguyen Duc Khoi <ndkhoi168@gmail.com>
2012-05-25 10:09:41 +0700

    Init Less Compiler

---
+++ main.js
@@ -0,0 +1,309 @@
+var lesstester = lesstester || {};
+
+lesstester.Log = function (selector)
+{
+   var self, data;
+
+   self = this;
+
+   data = {
+      container:$(selector),
+      lines:""
+   };
+
+   self.config = function (obj)
+   {
+      $.each(obj, function (key, value)
+      {
+         data[ key ] = value;
+      });
+   };
+
+   self.addMessage = function (msg)
+   {
+      var logElm = $("#log");
+      logElm.html(msg);
+   };
+
+   self.addError = function (msg)
+   {
+      self.addMessage(
+         '<strong class="label label-warning">ERROR</strong> {{msg}}'
+            .replace("{{msg}}", msg)
+      );
+   };
+
+   self.addCompiled = function (msg)
+   {
+      self.addMessage(
+         '<strong class="label label-success">COMPILE</strong> {{msg}}'
+            .replace("{{msg}}", msg)
+      );
+   };
+
+   return self;
+
+};
+/**
+ * Editor class
+ * ( lesstester.editor )
+ * @require jquery.js
+ */
+
+var lesstester = lesstester || {};
+
+lesstester.Editor = function (selector)
+{
+
+   var self, $textarea, textarea, data, methods;
+
+   self = this;
+   self.appName = "Editor";
+   self.selector = selector;
+
+   $textarea = $(selector);
+   textarea = $textarea.get(0);
+
+   data = {
+      msie:navigator.userAgent.match(/MSIE\s(.+?);/) !== null,
+      LF:String.fromCharCode(10),
+      CR:String.fromCharCode(13),
+      TAB:String.fromCharCode(9),
+      onShiftEnter:null
+   };
+
+   methods = {
+      log:function (val)
+      {
+         console.log(val);
+      },
+      onKeyDown:function (e)
+      {
+         switch (e.keyCode)
+         {
+            case 9 : // tab
+               methods.insertString(data.TAB);
+               return false;
+               break;
+            case 13 : // return
+               if (!$textarea.val().length)
+               {
+                  return;
+               }
+               var isShift = e.shiftKey;
+               if (isShift && data.onShiftEnter !== null)
+               {
+                  data.onShiftEnter.apply(self);
+                  return false;
+               }
+               else
+               {
+                  var match = methods.getBeforeLine().match(RegExp("^[" + data.TAB + "]+"));
+                  if (match !== null)
+                  {
+                     methods.insertString(data.LF);
+                     methods.insertString(
+                        methods.getStringRepeat(data.TAB, ( match ) ? match[0].length : 0)
+                     );
+                     return false;
+                  }
+               }
+               break;
+            default :
+               break;
+         }
+      },
+      getCaretPosition:function ()
+      {
+         if (data.msie)
+         {
+            var range, src, pos, i;
+            textarea.focus();
+
+            // get position
+            range = document.selection.createRange();
+            range.moveStart("character", -textarea.value.length);
+            src = range.text;
+            pos = range.text.split("\r\n").join("\n").split("\n").join("/").length;
+
+            // check if the beginning of a line or not
+            i = pos;
+            while (i--)
+            {
+               range.moveEnd("character", -1);
+               if (( range.text.length - src.length ) == 0)
+               {
+                  pos += 1;
+               }
+               else
+               {
+                  break;
+               }
+            }
+
+            return pos;
+         }
+         else
+         {
+            return textarea.selectionStart;
+         }
+      },
+      setCaretPosition:function (pos)
+      {
+         if (data.msie)
+         {
+            var range;
+            range = textarea.createTextRange();
+            range.collapse();
+            range.moveEnd("character", pos);
+            range.moveStart("character", pos);
+            range.select();
+            return;
+         }
+         else
+         {
+            textarea.setSelectionRange(pos, pos);
+            return;
+         }
+      },
+      insertString:function (str)
+      {
+         var pos, val, leftStr, rightStr;
+         pos = methods.getCaretPosition() || 0;
+         val = $textarea.val();
+         $textarea.val(val.substr(0, pos) + str + val.slice(pos));
+         methods.setCaretPosition(pos + str.length);
+         return;
+      },
+      getBeforeLine:function ()
+      {
+         var lines;
+         lines = $textarea.val()
+            .substr(0, methods.getCaretPosition() || 0)
+            .split(data.LF);
+         return lines[ lines.length - 1 ];
+      },
+      getStringRepeat:function (str, count)
+      {
+         var result = "";
+         while (count--)
+         {
+            result += str;
+         }
+         return result;
+      }
+   };
+
+   self.config = function (obj)
+   {
+      $.each(obj, function (key, value)
+      {
+         data[ key ] = value;
+      });
+   };
+
+   self.val = function (value)
+   {
+      if (value)
+      {
+         $textarea.val(value);
+      }
+      return $textarea.val();
+   };
+
+   $textarea.bind("keydown", methods.onKeyDown);
+
+   return self;
+};
+/**
+ * Compiler class
+ * ( lesstester.Compiler )
+ * @require jquery.js, less.js
+ */
+
+var lesstester = lesstester || {};
+
+lesstester.Compiler = function ()
+{
+   var self, data, events;
+
+   self = this;
+
+   data = {
+      parser:new less.Parser(),
+      compress:false,
+      onComplete:function ()
+      {
+      },
+      onError:function ()
+      {
+      }
+   };
+
+   self.config = function (obj)
+   {
+      $.each(obj, function (key, value)
+      {
+         data[ key ] = value;
+      });
+   };
+
+   self.parse = function (less)
+   {
+      data.parser.parse(less, function (error, tree)
+      {
+         if (error)
+         {
+            data.onError(error);
+            return;
+         }
+         data.onComplete(tree.toCSS({ compress:data.compress }));
+      });
+   };
+
+   self.bind = function (name, handler)
+   {
+      $(self).bind(name, handler);
+   };
+
+
+   return self;
+
+};
+
+(function ($, undefined)
+{
+   var compiler, editor, log;
+   log = new lesstester.Log("#log");
+   compiler = new lesstester.Compiler();
+   compiler.config({
+      compress:false,
+      onComplete:function (css)
+      {
+         $("#result").text(css);
+         log.addCompiled("Successfully compiled!");
+      },
+      onError:function (error)
+      {
+         log.addError(error.message);
+      }
+
+   });
+
+   // editor
+   editor = new lesstester.Editor("#editor");
+   editor.config({
+      onShiftEnter:function ()
+      {
+         compiler.parse(this.val());
+      }
+   });
+
+// Compile button
+   $("#button-compile").click(function ()
+   {
+      compiler.parse(editor.val());
+   });
+
+
+})(jQuery, undefined);
